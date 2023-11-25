4c7cb17c1ed1cd43312c973e722d3261bea91775
deyvisongdias <deyvisondias@hotmail.com>
2023-11-25 16:14:53 -0300

    corrigida funções do main.js

---
+++ src/main.js
@@ -0,0 +1,33 @@
+let joao = {
+    tipo: "maçã",
+    qtd: 1
+}
+
+let maria = {
+    tipo: "maçã",
+    qtd: 2
+}
+
+function deJoaoParaMaria() {
+    maria.qtd=maria.qtd+joao.qtd;
+    joao.qtd=0;
+}
+function deMariaParaJoao() {
+    joao.qtd = joao.qtd+maria.qtd;
+    maria.qtd=0;
+}
+function getMaria() {
+    return maria.qtd;
+}
+function getJoao() {
+    return joao.qtd;
+}
+function setMaria(value) {
+    maria.qtd = value;
+}
+
+function setJoao(value) {
+    joao.qtd = value;
+}
+
+export { deJoaoParaMaria, deMariaParaJoao, getMaria, getJoao, setMaria, setJoao };
